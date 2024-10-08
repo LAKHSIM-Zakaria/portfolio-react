8bcb1dd365f1a6230971d1b47eaeec054dd1bf7e
lakhsim <zakaria.lakhsim@capgemini.com>
2024-10-08 21:52:39 +0100

    init

---
+++ src/app/page.jsx
@@ -0,0 +1,47 @@
+"use client";
+
+import Image from "next/image";
+import { motion } from "framer-motion";
+
+const Homepage = () => {
+  return (
+    <motion.div
+      className="h-full"
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
+    >
+      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+        {/* IMAGE CONTAINER */}
+        <div className="h-1/2 lg:h-full lg:w-1/2 relative" style={{ marginLeft: '-21%' }}>
+          <Image src="/herome-removebg.png" alt="" fill className="object-contain"/>
+        </div>
+        {/* TEXT CONTAINER */}
+        <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
+          {/* TITLE */}
+          <h1 className="text-4xl md:text-6xl font-bold">
+            Software Engineer.
+          </h1>
+          {/* DESC */}
+          <p className="md:text-xl">
+  I am a seasoned Software Engineer with a strong track record of success. 
+  Currently, I'm thriving in my role as a Software Engineer at Capgemini Engineering 
+  in Casablanca-Settat, Morocco, where I've gained valuable experience over the past 4 years.
+</p>
+
+          {/* BUTTONS */}
+          <div className="w-full flex gap-4">
+            <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
+              View My Work
+            </button>
+            <button className="p-4 rounded-lg ring-1 ring-black">
+              Contact Me
+            </button>
+          </div>
+        </div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default Homepage;
---
+++ src/app/portfolio/page.jsx
@@ -0,0 +1,119 @@
+"use client";
+import { motion, useScroll, useTransform } from "framer-motion";
+import Image from "next/image";
+import Link from "next/link";
+import { useRef } from "react";
+
+const items = [
+  {
+    id: 1,
+    color: "from-red-300 to-blue-300",
+    title: "E-commerce Application",
+    desc: "A mobile application as a mini-project: Sale of accessories and clothing.",
+    img: "https://images.pexels.com/photos/18073372/pexels-photo-18073372/free-photo-of-young-man-sitting-in-a-car-on-a-night-street.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    link: "/",
+  },
+  {
+    id: 2,
+    color: "from-blue-300 to-violet-300",
+    title: "medical Application",
+    desc: "LA web application for managing, tracking, and controlling the medical records of each patient. Prediction of symptoms for each patient (Mean Stack Project) Tools: Angular 8, Node.js, Express.js, MongoDB, Bootstrap 4, JavaScript.",
+    img: "/medical.jpg",
+    link: "/",
+  },
+  {
+    id: 3,
+    color: "from-violet-300 to-purple-300",
+    title: "Social media analysis platform",
+    desc: "This project involved collecting data from Twitter and YouTube using their APIs, storing the information in MySQL and MongoDB. Analysing social media trends with backend support from Spring Boot and data processing in Python, visualizing the findings through interactive charts in an Angular 9 frontend. This comprehensive approach enabled effective data collection, storage, and analysis of social media insights.",
+    img: "/social.jpg",
+    link: "/",
+  },
+  {
+    id: 4,
+    color: "from-purple-300 to-red-300",
+    title: "Weather Application",
+    desc: "Android weather application that provides real-time weather updates and forecasts. Users can search for locations to view current conditions, hourly forecasts, and alerts, all presented in a user-friendly interface. The app utilizes a weather API for accurate data and features an appealing design for easy navigation.",
+    img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    link: "/",
+  },
+];
+
+const PortfolioPage = () => {
+  const ref = useRef();
+
+  const { scrollYProgress } = useScroll({ target: ref });
+  const x = useTransform(scrollYProgress, [0, 1], ["0%", "-80%"]);
+
+  return (
+    <motion.div
+      className="h-full"
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
+    >
+      <div className="h-[600vh] relative" ref={ref}>
+        <div className="w-screen h-[calc(100vh-6rem)] flex items-center justify-center text-8xl text-center">
+          My Works
+        </div>
+        <div className="sticky top-0 flex h-screen gap-4 items-center overflow-hidden">
+          <motion.div style={{ x }} className="flex">
+            <div className="h-screen w-screen flex items-center justify-center bg-gradient-to-r from-purple-300 to-red-300" />
+            {items.map((item) => (
+              <div
+                className={`h-screen w-screen flex items-center justify-center bg-gradient-to-r ${item.color}`}
+                key={item.id}
+              >
+                <div className="flex flex-col gap-8 text-white">
+                  <h1 className="text-xl font-bold md:text-4xl lg:text-6xl xl:text-8xl">
+                    {item.title}
+                  </h1>
+                  <div className="relative w-80 h-56 md:w-96 md:h-64 lg:w-[500px] lg:h-[350px] xl:w-[600px] xl:h-[420px]">
+                    <Image src={item.img} alt="" fill />
+                  </div>
+                  <p className="w-80 md:w96 lg:w-[500px] lg:text-lg xl:w-[600px]">
+                    {item.desc}
+                  </p>
+                  <Link href={item.link} className="flex justify-end">
+                    <button className="p-2 text-sm md:p-4 md:text-md lg:p-8 lg:text-lg bg-white text-gray-600 font-semibold m-4 rounded">See Demo</button>
+                  </Link>
+                </div>
+              </div>
+            ))}
+          </motion.div>
+        </div>
+      </div>
+      <div className="w-screen h-screen flex flex-col gap-16 items-center justify-center text-center">
+        <h1 className="text-8xl">Do you have a project?</h1>
+        <div className="relative">
+          <motion.svg
+            animate={{ rotate: 360 }}
+            transition={{ duration: 8, ease: "linear", repeat: Infinity }}
+            viewBox="0 0 300 300"
+            className="w-64 h-64 md:w-[500px] md:h-[500px] "
+          >
+            <defs>
+              <path
+                id="circlePath"
+                d="M 150, 150 m -60, 0 a 60,60 0 0,1 120,0 a 60,60 0 0,1 -120,0 "
+              />
+            </defs>
+            <text fill="#000">
+              <textPath xlinkHref="#circlePath" className="text-xl">
+                Front-end and mobile developer
+              </textPath>
+            </text>
+          </motion.svg>
+          <Link
+            href="/contact"
+            className="w-16 h-16 md:w-28 md:h-28 absolute top-0 left-0 right-0 bottom-0 m-auto bg-black text-white rounded-full flex items-center justify-center"
+          >
+            Hire Me
+          </Link>
+        </div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default PortfolioPage;
