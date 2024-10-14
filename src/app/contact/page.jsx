28e1318e6eb98093fa741828a0faff836d6ab49e
lakhsim <zakaria.lakhsim@capgemini.com>
2024-10-14 21:17:36 +0100

    fix

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -42,42 +42,45 @@ const ContactPage = () => {
     >
       <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
         {/* TEXT CONTAINER */}
-        <div className="h-1/2 lg:h-full lg:w-1/2 flex items-center justify-center text-6xl">
-          <div>
-            {text.split("").map((letter, index) => (
-              <motion.span
-                key={index}
-                initial={{ opacity: 1 }}
-                animate={{ opacity: 0 }}
-                transition={{
-                  duration: 3,
-                  repeat: Infinity,
-                  delay: index * 0.1,
-                }}
-              >
-                {letter}
-              </motion.span>
-            ))}
-            😊
-          </div>
-        </div>
+        <div className="h-1/4 lg:h-1/2 lg:w-1/2 flex items-center justify-center text-6xl">
+  <div>
+    {text.split("").map((letter, index) => (
+      <motion.span
+        key={index}
+        initial={{ opacity: 1 }}
+        animate={{ opacity: 0 }}
+        transition={{
+          duration: 3,
+          repeat: Infinity,
+          delay: index * 0.1,
+        }}
+      >
+        {letter}
+      </motion.span>
+    ))}
+    😊
+  </div>
+</div>
+
         {/* FORM CONTAINER */}
         <form
           onSubmit={sendEmail}
           ref={form}
-          className="h-1/2 lg:h-full lg:w-1/2 bg-red-50 rounded-xl text-xl flex flex-col gap-8 justify-center p-24"
+          className="h-full lg:h-full lg:w-1/2 bg-red-50 rounded-xl text-xl flex flex-col gap-6 justify-center p-8 sm:p-16"
         >
           <span>Dear Zakaria,</span>
           <textarea
             rows={6}
-            className="bg-transparent border-b-2 border-b-black outline-none resize-none"
+            className="bg-transparent border-b-2 border-b-black outline-none p-2"
             name="user_message"
+            placeholder="Type your message here..." // Added a placeholder for better user experience
           />
           <span>My mail address is:</span>
           <input
             name="user_email"
             type="text"
-            className="bg-transparent border-b-2 border-b-black outline-none"
+            className="bg-transparent border-b-2 border-b-black outline-none p-2"
+            placeholder="your.email@example.com" // Added a placeholder for better user experience
           />
           <span>Regards</span>
           <button className="bg-purple-200 rounded font-semibold text-gray-600 p-4">
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import { motion } from "framer-motion";
+import Link from "next/link";
 
 const Homepage = () => {
   return (
@@ -11,10 +12,16 @@ const Homepage = () => {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+      <div className="h-full flex flex-col lg:flex-row justify-center items-center px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
         {/* IMAGE CONTAINER */}
-        <div className="h-1/2 lg:h-full lg:w-1/2 relative" style={{ marginLeft: '-21%' }}>
-          <Image src="/herome-removebg.png" alt="" fill className="object-contain"/>
+        <div className="h-1/2 lg:h-1/2 lg:w-1/2 relative">
+          <Image
+            src="/herome-removebg-r.png"
+            width={400}
+            height={400}
+            className="object-cover"
+            style={{ height: "auto", maxHeight: "400px" }} // Use object-cover to maintain aspect ratio
+          />
         </div>
         {/* TEXT CONTAINER */}
         <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
@@ -24,19 +31,19 @@ const Homepage = () => {
           </h1>
           {/* DESC */}
           <p className="md:text-xl">
-  I am a seasoned Software Engineer with a strong track record of success. 
-  Currently, I am thriving in my role as a Software Engineer at Capgemini Engineering 
-  in Casablanca-Settat, Morocco, where I have gained valuable experience over the past 4 years.
-</p>
+            I am a seasoned Software Engineer with a strong track record of success. 
+            Currently, I am thriving in my role as a Software Engineer at Capgemini Engineering 
+            in Casablanca-Settat, Morocco, where I have gained valuable experience over the past 4 years.
+          </p>
 
           {/* BUTTONS */}
           <div className="w-full flex gap-4">
-            <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
+            <Link href ="/portfolio" className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
               View My Work
-            </button>
-            <button className="p-4 rounded-lg ring-1 ring-black">
+            </Link>
+            <Link href ="/contact" className="p-4 rounded-lg ring-1 ring-black">
               Contact Me
-            </button>
+            </Link>
           </div>
         </div>
       </div>
